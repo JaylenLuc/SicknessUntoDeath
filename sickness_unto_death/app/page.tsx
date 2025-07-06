e468159447e816f74240253a026e50f513c0e8ce
JaylenLuc <jhluc@uci.edu>
2025-07-06 06:09:30 +0000

    ok

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2">Let the wind carry</Text>
-      <div className= "grid grid-cols-3 items-center">
+      <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-8">
         <ArtCon />
         <MindBlown />
       </div>
