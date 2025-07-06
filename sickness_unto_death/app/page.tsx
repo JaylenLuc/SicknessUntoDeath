a2840861b5e0edc638ae07bc425751bcdba75c23
JaylenLuc <jhluc@uci.edu>
2025-07-06 06:03:00 +0000

    ugh

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -1,10 +1,12 @@
 import { Text } from "@/components/retroui/Text";
 import ArtCon from "@/components/main_page/artCon";
+import MindBlown from "@/components/main_page/mindBlown";
 export default function Home() {
   return (
-    <div className="grid grid-cols-1 grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+    <div className="grid grid-cols-3 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2">Let the wind carry</Text>
       <ArtCon />
+      <MindBlown />
     </div>
   );
 }
---
+++ sickness_unto_death/components/main_page/mindBlown.tsx
@@ -0,0 +1,22 @@
+import StopMotion from "../stopMotion";
+
+export default function MindBlown() {
+  const images = [
+    "/mindBlown/mind_explosion1.png",
+    "/mindBlown/mind_explosion1-5.png",
+    "/mindBlown/mind_explosion2.png",
+    "/mindBlown/mind_explosion3.png",
+    "/mindBlown/mind_explosion.png",
+    "/mindBlown/mind_explosion3.png",
+    "/mindBlown/mind_explosion2.png",
+    "/mindBlown/mind_explosion1-5.png",
+  ];
+
+  return (
+      <StopMotion
+        images={images}
+        frameDuration={400}
+        size="lg"
+      />
+  );
+}
