276e9833f696bcef49de43f11b499d76ec75331c
JaylenLuc <jhluc@uci.edu>
2025-07-06 06:04:07 +0000

    ugh

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -3,10 +3,12 @@ import ArtCon from "@/components/main_page/artCon";
 import MindBlown from "@/components/main_page/mindBlown";
 export default function Home() {
   return (
-    <div className="grid grid-cols-3 items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+    <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2">Let the wind carry</Text>
-      <ArtCon />
-      <MindBlown />
+      <div className= "grid grid-cols-3 items-center">
+        <ArtCon />
+        <MindBlown />
+      </div>
     </div>
   );
 }
