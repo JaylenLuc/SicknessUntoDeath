bc6f5fb21fa2291dbdf6fb13eb4148a271d1eb99
JaylenLuc <jhluc@uci.edu>
2025-07-10 06:37:53 +0000

    temp

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -2,16 +2,10 @@ import { Text } from "@/components/retroui/Text";
 import ArtCon from "@/components/main_page/artCon";
 import MindBlown from "@/components/main_page/mindBlown";
 import Link from "next/link";
-export default function Home() {
+export default function Dreams() {
   return (
     <div className="justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
-      <Text as="h2" className="text-center">Let the wind carry</Text>
-      <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-16">
-        <ArtCon />
-        <Link href="/dreams">
-          <MindBlown />
-        </Link>
-      </div>
+        Dreams
     </div>
   );
 }
