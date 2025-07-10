81ee48246919942bbea04d2dfb23da194d1f65ea
JaylenLuc <jhluc@uci.edu>
2025-07-10 07:41:06 +0000

    lalala

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -4,9 +4,9 @@ import MindBlown from "@/components/main_page/mindBlown";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="justify-items-center min-h-screen sm:p-20 p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+    <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2" className="text-center">Let the wind carry</Text>
-      <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-16">
+      <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-8">
         <ArtCon />
         <Link href="/dreams">
           <MindBlown />
