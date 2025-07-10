426e78b1f7045d8a8789c14fe5d81bcd402eadaf
JaylenLuc <jhluc@uci.edu>
2025-07-10 06:38:57 +0000

    fix

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -1,7 +1,3 @@
-import { Text } from "@/components/retroui/Text";
-import ArtCon from "@/components/main_page/artCon";
-import MindBlown from "@/components/main_page/mindBlown";
-import Link from "next/link";
 export default function Dreams() {
   return (
     <div className="justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
