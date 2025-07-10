2d8a234a3465d56a851b7ed2ef49fee2fa215a95
JaylenLuc <jhluc@uci.edu>
2025-07-10 06:56:42 +0000

    padding

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -4,7 +4,7 @@ import MindBlown from "@/components/main_page/mindBlown";
 import Link from "next/link";
 export default function Home() {
   return (
-    <div className="justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+    <div className="justify-items-center min-h-screen sm:p-20 p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2" className="text-center">Let the wind carry</Text>
       <div className="grid grid-cols-1 sm:grid-cols-3 items-center gap-16">
         <ArtCon />
