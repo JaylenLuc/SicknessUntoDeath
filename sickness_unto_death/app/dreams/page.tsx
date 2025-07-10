350d794ccea4b16f599c33a4ee1b8b9e2f4b17db
JaylenLuc <jhluc@uci.edu>
2025-07-10 07:46:28 +0000

    f

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -3,19 +3,20 @@ import Image from 'next/image';
 
 export default function Dreams() {
   return (
-    <div className="justify-items-center min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
-        Dreams
-        <div className="fixed bottom-0 left-1/2 -translate-x-1/2 w-144 h-144">
-            <Image
-                key={0}
-                src={"/mindBlown/dream1.png"}
-                alt={`frame-0`}
-                fill
-                className={cn(
-                "object-contain transition-opacity duration-0"
-                )}
-            />
-        </div>
+    <div className="flex flex-col min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+      Dreams
+      <div className="flex-1" />
+      <div className="mx-auto w-96 h-96">
+        <Image
+          key={0}
+          src={"/mindBlown/dream1.png"}
+          alt={`frame-0`}
+          fill
+          className={cn(
+            "object-contain transition-opacity duration-0"
+          )}
+        />
+      </div>
     </div>
   );
 }
