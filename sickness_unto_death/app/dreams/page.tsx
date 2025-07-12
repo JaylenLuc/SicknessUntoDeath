26ee93f16461d28a6486478644ba27688d0db634
JaylenLuc <jhluc@uci.edu>
2025-07-12 11:35:42 +0000

    hi

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -1,11 +1,12 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
-
+import DreamBubble from '@/components/dreams_page/dreamBubble';
 export default function Dreams() {
   return (
     <div className="flex flex-col min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       Dreams
       <div className="flex-1" />
+      <DreamBubble/>
       <div className="mx-auto w-96 h-96">
         <Image
           key={0}
@@ -13,7 +14,7 @@ export default function Dreams() {
           alt={`frame-0`}
           fill
           className={cn(
-            "object-contain transition-opacity duration-0"
+            "object-contain transition-opacity duration-0 z-10"
           )}
         />
       </div>
---
+++ sickness_unto_death/components/dreams_page/dreamBubble.tsx
@@ -0,0 +1,26 @@
+import StopMotion from "../stopMotion";
+
+export default function DreamBubble() {
+  const images = [
+    "/mindBlown/cloud1.png",
+    "/mindBlown/cloud2.png",
+    "/mindBlown/cloud3.png",
+    "/mindBlown/cloud4.png",
+    "/mindBlown/cloud_last.png",
+    "/mindBlown/cloud_last_2.png",
+    "/mindBlown/cloud_last_3.png",
+    "/mindBlown/cloud_last_2.png",
+    "/mindBlown/cloud_last.png",
+    "/mindBlown/cloud4.png",
+    "/mindBlown/cloud3.png",
+    "/mindBlown/cloud2.png",
+  ];
+
+  return (
+      <StopMotion
+        images={images}
+        frameDuration={400}
+        size="lg"
+      />
+  );
+}
