6d42362e68a678653d329f3ca90f9b593d86a195
JaylenLuc <jhluc@uci.edu>
2025-07-13 18:29:41 +0000

    sad

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -5,20 +5,22 @@ import { Text } from "@/components/retroui/Text";
 export default function Dreams() {
   return (
     <div className="flex flex-col justify-center min-h-screen sm:p-8 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
-      <Text>As deep as the Ocean, As grounded as the Soil</Text>
+      <Text as="h3">As deep as the Ocean, As grounded as the Soil</Text>
       <div className="flex-1 flex flex-col">
         <div className="flex justify-center">
           <DreamBubble />
         </div>
+        <div className="relative mx-auto w-full max-w-md aspect-square mt-auto mb-8">
           <Image
             key={0}
             src={"/mindBlown/dream1.png"}
             alt={`frame-0`}
             fill
             className={cn(
-              "transition-opacity object-contain duration-0 z-10"
+              "object-contain z-10"
             )}
           />
+        </div>
       </div>
     </div>
   );
