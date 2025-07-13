def4466ddec01566df597b44f1ed65654a6f8104
JaylenLuc <jhluc@uci.edu>
2025-07-13 05:10:50 +0000

    startedbow

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -1,22 +1,24 @@
 import { cn } from '@/lib/utils';
 import Image from 'next/image';
 import DreamBubble from '@/components/dreams_page/dreamBubble';
+import { Text } from "@/components/retroui/Text";
 export default function Dreams() {
   return (
-    <div className="flex flex-col min-h-screen sm:p-20 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
-      Dreams
-      <div className="flex-1" />
-      <DreamBubble/>
-      <div className="mx-auto w-96 h-96">
-        <Image
-          key={0}
-          src={"/mindBlown/dream1.png"}
-          alt={`frame-0`}
-          fill
-          className={cn(
-            "object-contain transition-opacity duration-0 z-10"
-          )}
-        />
+    <div className="flex flex-col items-center min-h-screen sm:p-2 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
+      <Text as="h2" className="text-center">As Deep as the Ocean, As Grounded as the Soil</Text>
+      <DreamBubble />
+      <div className="flex flex-col items-center flex-1 justify-end">
+        <div className="w-96 h-96">
+          <Image
+            key={0}
+            src={"/mindBlown/dream1.png"}
+            alt={`frame-0`}
+            fill
+            className={cn(
+              "object-contain transition-opacity duration-0 z-10"
+            )}
+          />
+        </div>
       </div>
     </div>
   );
