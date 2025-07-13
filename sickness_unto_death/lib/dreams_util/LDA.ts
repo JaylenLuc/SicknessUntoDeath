67d399573061d4e55691641543ab01ed3f7630b7
JaylenLuc <jhluc@uci.edu>
2025-07-13 05:20:05 +0000

    get rid of linting errors

--- sickness_unto_death/lib/dreams_util/LDA.ts
+++ sickness_unto_death/lib/dreams_util/LDA.ts
@@ -1,16 +1,16 @@
-import winkNLP from 'wink-nlp';
-import model from 'wink-eng-lite-web-model';
-export const bagOfWords= (text : string) => {
-    const nlp = winkNLP(model);
-    const doc = nlp.readDoc( text );
-    const sentences = doc.sentences().out();
-    const tokenizedSentences = tokenize(sentences);
+// import winkNLP from 'wink-nlp';
+// import model from 'wink-eng-lite-web-model';
+// export const bagOfWords= (text : string) => {
+//     const nlp = winkNLP(model);
+//     const doc = nlp.readDoc( text );
+//     const sentences = doc.sentences().out();
+//     const tokenizedSentences = tokenize(sentences);
 
-}
+// }
 
-const tokenize = (text: string[]) => {
+// const tokenize = (text: string[]) => {
 
-}
+// }
 
-const testText = " I had a dream that I was flying over a vast ocean, the waves sparkling under the sun. Suddenly, I found myself on a beach, feeling the warm sand beneath my feet. The sky was clear, and I could see distant mountains in the horizon.";
-console.log(bagOfWords(testText));
+// const testText = " I had a dream that I was flying over a vast ocean, the waves sparkling under the sun. Suddenly, I found myself on a beach, feeling the warm sand beneath my feet. The sky was clear, and I could see distant mountains in the horizon.";
+// console.log(bagOfWords(testText));
