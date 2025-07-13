3b990ca07b25dd2cfbed75838bb5e7b202c1dee2
JaylenLuc <jhluc@uci.edu>
2025-07-13 05:59:09 +0000

    g

--- sickness_unto_death/app/dreams/page.tsx
+++ sickness_unto_death/app/dreams/page.tsx
@@ -6,9 +6,11 @@ export default function Dreams() {
   return (
     <div className="flex flex-col items-center min-h-screen sm:p-2 font-[family-name:var(--font-courier-prime)] bg-[#d2b48c] overflow-hidden">
       <Text as="h2" className="text-center">As Deep as the Ocean, As Grounded as the Soil</Text>
-      <div className="flex flex-col pt-4 items-center flex-1 justify-end">
-        <DreamBubble />
-        <div className="w-96 h-96">
+      <div className="flex flex-col pt-8 items-center flex-1 justify-end">
+        <div className="mb-16">
+          <DreamBubble/>
+        </div>
+        <div className="w-96 h-96 mt-8">
           <Image
             key={0}
             src={"/mindBlown/dream1.png"}
