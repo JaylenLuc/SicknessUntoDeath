8015eb6a3bf36fbbbed9ba3c83894ef8546c8273
JaylenLuc <jhluc@uci.edu>
2025-07-31 09:20:10 +0000

    moon gif

--- sickness_unto_death/app/page.tsx
+++ sickness_unto_death/app/page.tsx
@@ -2,6 +2,7 @@ import { Text } from "@/components/retroui/Text";
 import ArtCon from "@/components/main_page/artCon";
 import MindBlown from "@/components/main_page/mindBlown";
 import Link from "next/link";
+import Image from "next/image";
 export default function Home() {
   return (
     <div className="justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-courier-prime)]">
@@ -11,6 +12,15 @@ export default function Home() {
         <Link href="/dreams">
           <MindBlown />
         </Link>
+        <div className = "relative overflow-hidden w-64 h-64">
+          <Image 
+            src="/mindBlown/MOONGif2.gif" 
+            alt="MOOOOOOO" 
+            unoptimized={true}
+            fill
+            className="inset-0 object-contain transition-opacity duration-0"
+          />
+        </div>
       </div>
     </div>
   );
